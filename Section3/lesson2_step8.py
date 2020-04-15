74467aadac21077181b72f6b39d090641939d3c1
Georgy Rafaelov <Georgy.Rafaelov@kaspersky.com>
2020-04-15 16:22:04 +0300

    Scripts of lesson4 of Section3 are added

---
+++ Section3/lesson2_step8.py
@@ -0,0 +1,8 @@
+def test_input_text(expected_result, actual_result):
+    # ваша реализация, напишите assert и сообщение об ошибке
+    expected = expected_result
+    actual = actual_result
+    assert expected == actual, f"expected {expected}, got {actual}"
+
+
+test_input_text(5, 8)
